55b47eaf0bf5b46e26c9238de005a17c5da4976a
gugut-net <faisel.ali1@yahoo.com>
2022-11-17 23:00:14 -0600

    first commit

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.myapp;
 
 import android.net.ParseException;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.parse.GetCallback;
 import com.parse.Parse;
+import com.parse.ParseAnalytics;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
@@ -26,28 +29,43 @@ public class MainActivity extends AppCompatActivity {
                 .server(getString(R.string.back4app_server_url))
                 .build()
         );
+//
+//        //Saving your First data object on Back4App
+//        ParseObject person = new ParseObject("Person");
+//        person.put("name", "Faisel Doe");
+//        person.put("age", 27);
+//        person.saveInBackground();
+//
+////Reading your First Data Object from Back4App
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Person");
+//        query.getInBackground("mhPFDlCahj", new GetCallback<ParseObject>() {
+//            @Override
+//            public void done(ParseObject object, com.parse.ParseException e) {
+//
+//            }
+//
+//            public void done(ParseObject object, ParseException e) {
+//                if (e == null) {
+//                    // object will be your person
+//                } else {
+//                    // something went wrong
+//                }
+//            }
+//        });
+//    }
+//        TextView textView = findViewById(R.id.textView);
+//        ParseObject firstObject = new  ParseObject("FirstClass");
+//        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+//        firstObject.saveInBackground(e -> {
+//            if (e != null){
+//                Log.e("MainActivity", e.getLocalizedMessage());
+//            }else{
+//                Log.d("MainActivity","Object saved.");
+//                textView.setText(String.format("Object saved. %s", firstObject.getObjectId()));
+//            }
+//        });
 
-        //Saving your First data object on Back4App
-        ParseObject person = new ParseObject("Person");
-        person.put("name", "John Snow");
-        person.put("age", 27);
-        person.saveInBackground();
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
-//Reading your First Data Object from Back4App
-        ParseQuery<ParseObject> query = ParseQuery.getQuery("Person");
-        query.getInBackground("mhPFDlCahj", new GetCallback<ParseObject>() {
-            @Override
-            public void done(ParseObject object, com.parse.ParseException e) {
-
-            }
-
-            public void done(ParseObject object, ParseException e) {
-                if (e == null) {
-                    // object will be your person
-                } else {
-                    // something went wrong
-                }
-            }
-        });
     }
 }
